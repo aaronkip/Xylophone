aebf01adc29058ead4f05a9c01d44dd7902afcba
Akron254 <aaronrono42@gmail.com>
2020-08-26 17:08:29 +0300

    Created Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,58 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  void makeSound() {}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {},
+                color: Colors.red,
+                child: Text(''),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.red,
+                child: Text(''),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.red,
+                child: Text(''),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.red,
+                child: Text(''),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.red,
+                child: Text(''),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.red,
+                child: Text(''),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.red,
+                child: Text(''),
+              ),
+            ],
+          ),
         ),
       ),
     );
